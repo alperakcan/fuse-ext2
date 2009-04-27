2e519b85c6060be8839c3403f25d1ee673d78842
anhanguera <anhanguera@1e10e969-9346-0410-851e-9bf269e4b554>
2009-04-27 19:58:53 +0000

    initial prefpane

--- trunk/tools/macosx/10.5/prefpane/fuse_ext2Pref.m
+++ trunk/tools/macosx/10.5/prefpane/fuse_ext2Pref.m
@@ -8,11 +8,53 @@
 
 #import "fuse_ext2Pref.h"
 
+static NSString *kaboutLabelString = @"fuse-ext2 is a ext2/ext3 filesystem support for Fuse. Please visit fuse-ext2 homepage for more information.";
+static NSString *kinstalledString = @"Installed Version:";
+static NSString *kupdateString = @"No Updates Available At This Time";
+
+@interface fuse_ext2Pref (PrivateMethods)
+
+- (NSString *) installedVersion;
+- (void) updateGUI;
+
+@end
 
 @implementation fuse_ext2Pref
 
-- (void) mainViewDidLoad
+- (IBAction) updateButtonClicked: (id) sender
 {
 }
+
+- (IBAction) removeButtonClicked: (id) sender
+{
+}
+
+- (NSString *) installedVersion
+{
+	NSString *versionString;
+	versionString = [[NSString alloc] initWithFormat:@"%@ %@", kinstalledString, @"0.0.3"];
+	return versionString;
+}
+
+- (void) updateGUI
+{
+	NSString *version;
+	version = [self installedVersion];
+	[aboutLabel setStringValue:kaboutLabelString];
+	[installedLabel setStringValue:version];
+	[updateLabel setStringValue:kupdateString];
+	[version release];
+}
+
+- (void) mainViewDidLoad
+{
+	[self updateGUI];
+}
+
+@synthesize aboutLabel;
+@synthesize removeButton;
+@synthesize updateButton;
+@synthesize installedLabel;
+@synthesize updateLabel;
 
 @end
