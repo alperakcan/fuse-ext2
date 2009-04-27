c7bdaed4c39211b6252a98a39bec21f8fcfa7a9d
anhanguera <anhanguera@1e10e969-9346-0410-851e-9bf269e4b554>
2009-04-27 19:36:44 +0000

    initial prefpane

---
+++ trunk/tools/macosx/10.5/prefpane/fuse_ext2Pref.h
@@ -0,0 +1,19 @@
+//
+//  fuse_ext2Pref.h
+//  fuse-ext2
+//
+//  Created by Alper Akcan on 4/27/09.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+
+@interface fuse_ext2Pref : NSPreferencePane 
+{
+
+}
+
+- (void) mainViewDidLoad;
+
+@end
