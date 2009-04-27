8aa51cd9feb4061dbf06279c9b961add289b115f
Alper Akcan <alper.akcan@gmail.com>
2009-04-27 19:36:44 +0000

    initial prefpane

---
+++ tools/macosx/10.5/prefpane/fuse_ext2Pref.m
@@ -0,0 +1,18 @@
+//
+//  fuse_ext2Pref.m
+//  fuse-ext2
+//
+//  Created by Alper Akcan on 4/27/09.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "fuse_ext2Pref.h"
+
+
+@implementation fuse_ext2Pref
+
+- (void) mainViewDidLoad
+{
+}
+
+@end
