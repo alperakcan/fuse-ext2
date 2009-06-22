77736451964f8cfaab9979559b752b9e601cc2cb
anhanguera <anhanguera@1e10e969-9346-0410-851e-9bf269e4b554>
2009-06-22 14:42:22 +0000

    update copyright

--- trunk/tools/macosx/fuse-ext2.uninstall.c
+++ trunk/tools/macosx/fuse-ext2.uninstall.c
@@ -1,3 +1,21 @@
+/**
+ * Copyright (c) 2008-2009 Alper Akcan <alper.akcan@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (in the main directory of the fuse-ext2
+ * distribution in the file COPYING); if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,8 +37,10 @@ typedef struct f_s {
 } f_t;
 
 static f_t *files[] = {
+	&(f_t) {directory, "/Library/Filesystems/fuse-ext2.fs"},
 	&(f_t) {directory, "/System/Library/Filesystems/fuse-ext2.fs"},
 	&(f_t) {directory, "/Library/PreferencePanes/fuse-ext2.prefPane"},
+	&(f_t) {directory, "/System/Library/PreferencePanes/fuse-ext2.prefPane"},
 	&(f_t) {file, "/usr/local/bin/fuse-ext2"},
 	&(f_t) {file, "/usr/local/bin/fuse-ext2.wait"},
 	&(f_t) {file, "/usr/local/bin/fuse-ext2.probe"},
@@ -33,7 +53,7 @@ static f_t *files[] = {
 static int rm_file (const char *path)
 {
 	if (access(path, W_OK | W_OK | F_OK) != 0) {
-		printf("access failed for '%s'\n", path);
+		printf("access failed for '%s' (%s)\n", path, strerror(errno));
 		if (errno == ENOENT) {
 			return 0;
 		}
@@ -54,7 +74,10 @@ static int rm_directory (const char *path)
 	dp = opendir(path);
 	if (dp == NULL) {
 		printf("opendir() failed for '%s' (%s)\n", path, strerror(errno));
-		return 0;
+		if (errno == ENOENT) {
+			return 0;
+		}
+		return -1;
 	}
 	while ((current = readdir(dp)) != NULL) {
 		if (strcmp(current->d_name, ".") == 0 ||
