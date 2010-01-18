230a0f8c899d98ccbc64530e6fa3521c78cc7f9c
anhanguera <anhanguera@1e10e969-9346-0410-851e-9bf269e4b554>
2010-01-18 17:55:10 +0000

    copyright update

--- trunk/fuse-ext2/op_unlink.c
+++ trunk/fuse-ext2/op_unlink.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2008-2009 Alper Akcan <alper.akcan@gmail.com>
+ * Copyright (c) 2008-2010 Alper Akcan <alper.akcan@gmail.com>
  * Copyright (c) 2009 Renzo Davoli <renzo@cs.unibo.it>
  *
  * This program is free software; you can redistribute it and/or modify
