3db6a12962114447aad5131b5348a02b89ced1cf
Emanuel Haupt <ehaupt@critical.ch>
2018-02-20 14:02:55 +0100

    ENODATA is not defined on FreeBSD
    ENODATA is not defined on FreeBSD. Define to the value of ENOMSG.

--- fuse-ext2/fuse-ext2.h
+++ fuse-ext2/fuse-ext2.h
@@ -34,6 +34,10 @@
 #include <fuse.h>
 #include <ext2fs/ext2fs.h>
 
+#ifndef ENODATA
+#define ENODATA ENOMSG
+#endif
+
 #if !defined(FUSE_VERSION) || (FUSE_VERSION < 26)
 #error "***********************************************************"
 #error "*                                                         *"
